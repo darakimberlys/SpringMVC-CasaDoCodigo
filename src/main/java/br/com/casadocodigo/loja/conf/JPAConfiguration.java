39f1bcd43dd466e3c878f81a6fbac623dc8ce154
Dara Kimberly <darakimberlys@hotmail.com>
2020-09-21 17:30:11 -0300

    Functional fix to MySQL

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -25,14 +25,15 @@ public class JPAConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUsername("root");
         dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql://localhost/casadocodigo");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3307/casadocodigo?useSSL=false&serverTimezone=UTC");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         factoryBean.setDataSource(dataSource);
 
         Properties properties = new Properties();
+
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         properties.setProperty("hibernate.show_sql", "true");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
 
         factoryBean.setJpaProperties(properties);
         factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
